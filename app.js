88e35218b262fa3f50e8bcbffeb6bcc52d376015
Bransholm <kenneth@bransholm.dk>
2023-09-20 01:23:53 +0200

    Route updated to mysql

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function getUsersFromJSON() {
 
 // READ all users
 app.get("/users", async (request, response) => {
-  const query = "SELECT * FROM users ORDER BY name";
+  const query = "SELECT * FROM users ORDER BY id";
   connection.query(query, (error, results, fields) => {
     if (error) {
       console.log(error);
@@ -33,50 +33,70 @@ app.get("/users", async (request, response) => {
 });
 
 // READ one user
-app.get("/users/:id", async (request, response) => {
-  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-  const users = await getUsersFromJSON();
-  const user = users.find((user) => user.id === id);
-  response.json(user);
+app.get("/users/:id", (request, response) => {
+  const id = request.params.id;
+  const query = "SELECT * FROM users WHERE id=?;";
+  const values = [id];
+
+  connection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results[0]);
+    }
+  });
 });
 
 // CREATE user
-app.post("/users", async (request, response) => {
-  const newUser = request.body;
-  newUser.id = new Date().getTime();
-  console.log(newUser);
-
-  const users = await getUsersFromJSON();
-  users.push(newUser);
-  fs.writeFile("data.json", JSON.stringify(users));
-  response.json(users);
+app.post("/users", (request, response) => {
+  const user = request.body;
+  const query = "INSERT INTO users(name, mail, title, image) values(?,?,?,?);";
+  const values = [user.name, user.mail, user.title, user.image];
+  
+  connection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
 });
+
+
 
 // UPDATE user
-app.put("/users/:id", async (request, response) => {
-  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-  const users = await getUsersFromJSON();
-  let userToUpdate = users.find((user) => user.id === id);
-  const body = request.body;
-  userToUpdate.image = body.image;
-  userToUpdate.mail = body.mail;
-  userToUpdate.name = body.name;
-  userToUpdate.title = body.title;
+app.put("/users/:id", (request, response) => {
+  const id = request.params.id;
+  const user = request.body;
+  const query = "UPDATE users SET name=?, mail=?, title=?, image=?;";
+  const values = [user.name, user.mail, user.title, user.image];
 
-  fs.writeFile("data.json", JSON.stringify(users));
-  response.json(users);
+  connection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
 });
+
+
 
 // DELETE user
-app.delete("/users/:id", async (request, response) => {
-  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-  const users = await getUsersFromJSON();
-  // const newUsers = users.filter(user => user.id !== id);
-  const index = users.findIndex((user) => user.id === id);
-  users.splice(index, 1);
-  fs.writeFile("data.json", JSON.stringify(users));
-  response.json(users);
+app.delete("/users/:id", (request, response) => {
+  const id = request.params.id;
+  const query = "DELETE FROM users WHERE id=?;";
+  const values = [id];
+
+  connection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
 });
+  
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
